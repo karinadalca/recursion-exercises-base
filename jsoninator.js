718f8da770578ac6f561ae2279846c1a5674b9e7
Karina Dalca <karinadalca@gmail.com>
2017-05-12 09:45:06 -0700

    jsoninator tests passed

--- jsoninator.js
+++ jsoninator.js
@@ -5,7 +5,41 @@ const _ = require('underscore'); // the real one! :)
 // But you don't. So you're going to write it from scratch...
 
 const stringify = function(obj) {
-  // Youre code here
+  if(obj == null){
+    return 'null';
+  }
+
+  if(typeof obj == 'string') {
+    if(obj.length < 1){
+      return ''
+    } else if (obj.length >= 1){
+      return '\"' + obj + '\"';
+    }
+  }
+
+  if(typeof obj === 'object' && obj.toString() !== '[object Object]') {
+    if(obj.length < 1){
+      return '[]';
+    } else if(obj.length >= 1){
+      return '[' + _.map(obj, function (element)
+        { return stringify(element)}) + ']';
+    }
+  }
+
+  if(obj.toString() === '[object Object]'){
+    let output = '';
+    let key = Object.keys(obj);
+
+    if(key.length < 1){
+      return '{}';
+    } else if(key.length >= 1){
+      for(let i = 0; i < key.length; i++){
+        return '{' + stringify(key[i]) + ':' +  stringify(obj[key[i]]) + '}';
+      }
+    }
+  }
+
+  return obj.toString();
 };
 
 module.exports = {
